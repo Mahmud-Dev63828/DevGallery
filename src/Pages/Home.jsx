1518b0a4edf729e4943326c83a456665a9afe7ff
Mahmud-Dev63828 <mahmudul.hasan63828@gmail.com>
2025-06-21 19:49:58 +0600

    all task completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
     return (
         <div className="">
 
-            <button onClick={handleTheme} className={mode == "light" ? "bg-green-400 cursor-pointer text-black ml-180  py-3 px-8" : " bg-black py-3 cursor-pointer text-white px-8 ml-180"} >{mode}</button>
+            <button onClick={handleTheme} className={mode == "light" ? "bg-green-500 cursor-pointer text-black ml-180  py-3 px-8" : " bg-black py-3 cursor-pointer text-white px-8 ml-180"} >{mode}</button>
 
             <div className='flex'>
                 <Suspense fallback={'Loading.....'}>
